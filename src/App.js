729137a394ac6ede044bc3d1800fa27a5d23cece
gungun005 <g053780@gmail.com>
2023-11-04 19:49:36 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import{BrowserRouter as Router,Switch,Route } from "react-router-dom"
+import{BrowserRouter as Router,Route,Routes} from "react-router-dom"
 
 
 function App() {
   return (
     <Router>
     <div className="app">
-     <h1>Hi! Gungun here..</h1>
+     <Routes>
+      <Route path="/checkout"> 
+      <h1>Checkout</h1>
+      </Route>
+
+      <Route path="/login"> 
+      <h1>Login page</h1>
+      </Route>
+
+      <Route path="/"> 
+      <h1>Home page</h1>
+      </Route>
+     </Routes>
     </div>
     </Router>
   );
