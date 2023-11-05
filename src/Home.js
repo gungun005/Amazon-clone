0f5a5ac2ada4ae2d8c7fd5ba8785af7f0d157099
gungun005 <g053780@gmail.com>
2023-11-05 20:26:11 +0530

    updated src

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import img2 from '../src/Bannerphoto.jpg';
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
 import  images  from  "./slider-images";
+import Product from './Product';
 
 function Home() {
   return (
@@ -20,6 +21,42 @@ function Home() {
         </Slider>
         
         {/* <img style={{width:"100%", height:"1%"}} src={img2} alt="banner photo" /> */}
+        {/* product id,image ,rating,name */}
+        <Product 
+                    id = "12314"
+                    title = "Macbook Pro"
+                    price = {175000}
+                    rating = {5}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71YRSVXhgQL._SX679_.jpg"
+                />
+         <Product 
+                    id = "12315"
+                    title = "OnePlus Nord"
+                    price = {27000}
+                    rating = {5}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71gag816F7L._SL1500_.jpg"
+                />
+                 <Product 
+                    id = "12316"
+                    title = "Amazon Echo"
+                    price = {7500}
+                    rating = {4}
+                    image = "https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+                />
+                <Product 
+                    id = "12317"
+                    title = "Fit Bit"
+                    price = {11999}
+                    rating = {5}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+                />
+                     <Product 
+                    id = "12318"
+                    title = "OnePlus Q1 Series 4K"
+                    price = {85000}
+                    rating = {5}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71D89swgrvL._SL1500_.jpg"
+                />
     </div>
   )
 }
