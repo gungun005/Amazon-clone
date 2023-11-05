4c3dbb17d01aa3a823c1dba530ffa3abaf67d895
gungun005 <g053780@gmail.com>
2023-11-05 17:16:12 +0530

    updated src

--- src/Header.js
+++ src/Header.js
@@ -18,10 +18,34 @@ function Header() {
       <div className="header_search">
       <input type="text" className="header_searchInput" />
       <SearchIcon className="header_searchIcon"/>
-      {/* three links  */}
-      {/* basket icon with number of items added */}
       </div>
-    
+      {/* three links  */}
+      <div className="header_nav">
+        {/* 1st Link */}
+       <Link to ="/login" className="header_link">
+       <div className="header_option">
+       <span>hello Gungun</span>
+       <span>Sign in</span>
+       </div>
+       </Link>
+        {/* 2ND Link */}
+        <Link to ="/" className="header_link">
+       <div className="header_option">
+       <span>Returns</span>
+       <span>& Orders</span>
+       </div>
+       </Link>
+
+        {/* 3rd Link */}
+        <Link to ="/" className="header_link">
+       <div className="header_option">
+       <span>Your</span>
+       <span>Prime</span>
+       </div>
+       </Link>
+      {/* basket icon with number of items added */}
+     
+      </div>
     </nav>
   )
 }
